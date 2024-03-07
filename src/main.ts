6e8749914120ff17c4dd05288bf53adb4594abba
Karol Piskorz <karolpiskorz8@gmail.com>
2024-03-07 18:41:50 +0100

    add prototype code

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+const gap: number = 16;
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+const col1Element = document.querySelector<HTMLDivElement>("#col1")!;
+
+col1Element.setAttribute("style", `--gap: ${gap}px;`);
+
+const moveContainerElement = document.querySelector<HTMLDivElement>(
+  "#col1 #move-container"
+)!;
+
+moveContainerElement.innerHTML += moveContainerElement.innerHTML;
+
+let progress = 0;
+const maxProgress = (moveContainerElement.clientHeight + gap) / 2;
+
+function animation() {
+  progress++;
+  moveContainerElement.style.transform = `translate(0, -${progress}px)`;
+  if (progress > maxProgress) progress = 0;
+}
+
+setInterval(() => window.requestAnimationFrame(animation), 5);
