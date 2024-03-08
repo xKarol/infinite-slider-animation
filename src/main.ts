c40013ff26c3cb8885cfb75f4c91edb41009eaad
Karol Piskorz <karolpiskorz8@gmail.com>
2024-03-08 13:56:45 +0100

    use public path for images

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { InfiniteSlider } from "./infinite-slider";
 import "./style.css";
+import image1Url from "/1.avif";
+import image2Url from "/2.avif";
+import image3Url from "/3.avif";
+
+import image4Url from "/4.avif";
+import image5Url from "/5.avif";
+import image6Url from "/6.avif";
 
 new InfiniteSlider({
-  images: [
-    "https://images.unsplash.com/photo-1626808642875-0aa545482dfb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1617243876873-6cea4ea0b4eb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1549593076-9556c5abdc1f?q=80&w=1949&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  ],
+  images: [image1Url, image2Url, image3Url],
   rootElement: document.querySelector<HTMLDivElement>("#col1")!,
   direction: "down",
   gap: 16,
@@ -14,11 +17,7 @@ new InfiniteSlider({
 });
 
 new InfiniteSlider({
-  images: [
-    "https://images.unsplash.com/photo-1583364484025-7cd9ff624984?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1518895949257-7621c3c786d7?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1640420806289-e8088a1cfe6d?q=80&w=2008&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  ],
+  images: [image4Url, image5Url, image6Url],
   rootElement: document.querySelector<HTMLDivElement>("#col2")!,
   direction: "up",
   gap: 16,
